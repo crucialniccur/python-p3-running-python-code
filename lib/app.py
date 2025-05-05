ae04fe9624afd1d9a1b2e695ae76abb1ae37c0d2
Mati Joseph <crucialniccur@gmail.com>
2025-05-05 11:17:13 +0300

    modify print ending

--- lib/app.py
+++ lib/app.py
@@ -1,3 +1,10 @@
 print('Hello world')
 print("Hello sun!")
 print("Hello sky!")
+
+
+# modifying print ending
+
+print('Hello world!', end=" ")
+print('Hello world!', end="!! ")
+print('Hello world!', end="!!! \n")
